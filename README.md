be2e37fbac8b7711090f0ab9ed1ddf749566a61e
RivinduM <rivindum.15@cse.mrt.ac.lk>
2019-12-16 21:37:44 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# AssistedLivingSystem
+Ambient assisted living system with privacy protection. This project is developed as a FYP, UoM, 2019.
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="adl-recognition",
+    version="0.0.1",
+    author="Rivindu Madushan <rivindum.15@cse.mrt.ac.lk>, Danushka Madhuranga <danushka.15@cse.mrt.ac.lk>, Chathuranga Siriwwardana <chathuranga.15@cse.mrt.ac.lk>",
+    author_email="rivindum.15@cse.mrt.ac.lk",
+    description="Classification of Activities of Daily Living(ADL) using depth videos and audio",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/RivinduM/AssistedLivingSystem",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2.7',
+    include_package_data=True
+)
