41a1e5a50431dbe949189f1d50a4a7e30b8a1aff
RivinduM <RivinduM@users.noreply.github.com>
2019-12-17 08:25:28 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Classification of Activities of Daily Living(ADL) using depth videos and audio",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/RivinduM/AssistedLivingSystem",
+    url="https://github.com/RivinduM/AdlRecognition",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
