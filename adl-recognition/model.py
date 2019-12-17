575982e338afac00e7f76d8f68c2be07b1e63651
RivinduM <rivindum.15@cse.mrt.ac.lk>
2019-12-17 08:12:05 +0530

    Modified models to read from extracted zip

--- adl-recognition/model.py
+++ adl-recognition/model.py
@@ -1,7 +1,11 @@
 from keras.models import model_from_json
-
+import os.path
+import zipfile
 
 def getModel(path):
+    if not(os.path.isfile(path+'.h5') or os.path.isfile(path+'.json')):
+        zip = zipfile.ZipFile(path+'.zip')
+        zip.extractall()
     json_file = open(path+'.json', 'r')
     model_json = json_file.read()
     json_file.close()
